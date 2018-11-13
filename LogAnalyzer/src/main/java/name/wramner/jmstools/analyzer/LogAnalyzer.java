a5cf52700367fd819431742e4d127364fad5b5e6
Erik Wramner <erik.wramner@codemint.com>
2018-11-13 18:58:51 +0100

    Moved LogAnalyzer to new package

--- LogAnalyzer/src/main/java/name/wramner/jmstools/analyzer/LogAnalyzer.java
+++ LogAnalyzer/src/main/java/name/wramner/jmstools/analyzer/LogAnalyzer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package name.wramner.jmstools;
+package name.wramner.jmstools.analyzer;
 
 import java.awt.Color;
 import java.io.BufferedReader;
